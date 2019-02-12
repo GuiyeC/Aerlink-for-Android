c97c5ae6a1f39deefe1bd5fe55255455bc3d71de
Guillermo Cique Fernández <GuiyeC@gmail.com>
2019-02-12 20:57:08 +0100

    Set explicit type to UUIDs on ServiceContracs

--- wear/src/main/java/com/codegy/aerlink/service/battery/BASContract.kt
+++ wear/src/main/java/com/codegy/aerlink/service/battery/BASContract.kt
@@ -10,8 +10,8 @@ import java.util.*
 object BASContract: ServiceContract {
 
     // BAS - Battery Service
-    override val serviceUuid = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
-    val batteryLevelCharacteristicUuid = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb")
+    override val serviceUuid: UUID = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
+    val batteryLevelCharacteristicUuid: UUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb")
 
     override val characteristicsToSubscribe: List<CharacteristicIdentifier> = listOf(CharacteristicIdentifier(serviceUuid, batteryLevelCharacteristicUuid))
 
--- wear/src/main/java/com/codegy/aerlink/service/notifications/ANCSContract.kt
+++ wear/src/main/java/com/codegy/aerlink/service/notifications/ANCSContract.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import com.codegy.aerlink.connection.CharacteristicIdentifier
 import com.codegy.aerlink.service.ServiceContract
 import com.codegy.aerlink.service.ServiceManager
-import com.codegy.aerlink.utils.ServiceUtils
+import com.codegy.aerlink.utils.CommandHandler
 import java.util.*
 
+/**
+ * https://developer.apple.com/library/archive/documentation/CoreBluetooth/Reference/AppleNotificationCenterServiceSpecification/Introduction/Introduction.html
+ */
 object ANCSContract: ServiceContract {
 
     // ANCS - Apple Notification Center Service Profile
-    override val serviceUuid = UUID.fromString("7905f431-b5ce-4e99-a40f-4b1e122d00d0")
-    val notificationSourceCharacteristicUuid = UUID.fromString("9fbf120d-6301-42d9-8c58-25e699a21dbd")
-    val dataSourceCharacteristicUuid         = UUID.fromString("22eac6e9-24d6-4bb5-be44-b36ace7c7bfb")
-    val controlPointCharacteristicUuid       = UUID.fromString("69d1d8f3-45e1-49a8-9821-9bbdfdaad9d9")
+    override val serviceUuid: UUID = UUID.fromString("7905f431-b5ce-4e99-a40f-4b1e122d00d0")
+    val notificationSourceCharacteristicUuid: UUID = UUID.fromString("9fbf120d-6301-42d9-8c58-25e699a21dbd")
+    val dataSourceCharacteristicUuid: UUID         = UUID.fromString("22eac6e9-24d6-4bb5-be44-b36ace7c7bfb")
+    val controlPointCharacteristicUuid: UUID       = UUID.fromString("69d1d8f3-45e1-49a8-9821-9bbdfdaad9d9")
 
     override val characteristicsToSubscribe: List<CharacteristicIdentifier> = listOf(
             CharacteristicIdentifier(serviceUuid, notificationSourceCharacteristicUuid),
